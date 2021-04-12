15e254a47ce982eb6ab61c5d25e2a48fb7966652
Clement-Muth <clementmuth@gmail.com>
2021-04-12 16:48:29 +0200

    feat: add all

---
+++ CMakeFiles/superBonobros2.dir/DependInfo.cmake
@@ -0,0 +1,60 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/cmuth/Github/G-JAM-001-STG-0-1-jam-louis.kleiver/Game/main.cpp" "/home/cmuth/Github/G-JAM-001-STG-0-1-jam-louis.kleiver/CMakeFiles/superBonobros2.dir/Game/main.cpp.o"
+  "/home/cmuth/Github/G-JAM-001-STG-0-1-jam-louis.kleiver/GameEngine/Clock/StdClock.cpp" "/home/cmuth/Github/G-JAM-001-STG-0-1-jam-louis.kleiver/CMakeFiles/superBonobros2.dir/GameEngine/Clock/StdClock.cpp.o"
+  "/home/cmuth/Github/G-JAM-001-STG-0-1-jam-louis.kleiver/GameEngine/Collision/SAT.cpp" "/home/cmuth/Github/G-JAM-001-STG-0-1-jam-louis.kleiver/CMakeFiles/superBonobros2.dir/GameEngine/Collision/SAT.cpp.o"
+  "/home/cmuth/Github/G-JAM-001-STG-0-1-jam-louis.kleiver/GameEngine/Command/ASceneCommandInterpreter.cpp" "/home/cmuth/Github/G-JAM-001-STG-0-1-jam-louis.kleiver/CMakeFiles/superBonobros2.dir/GameEngine/Command/ASceneCommandInterpreter.cpp.o"
+  "/home/cmuth/Github/G-JAM-001-STG-0-1-jam-louis.kleiver/GameEngine/Components/SkeletonSonsComponent.cpp" "/home/cmuth/Github/G-JAM-001-STG-0-1-jam-louis.kleiver/CMakeFiles/superBonobros2.dir/GameEngine/Components/SkeletonSonsComponent.cpp.o"
+  "/home/cmuth/Github/G-JAM-001-STG-0-1-jam-louis.kleiver/GameEngine/Damagable/Damagable.cpp" "/home/cmuth/Github/G-JAM-001-STG-0-1-jam-louis.kleiver/CMakeFiles/superBonobros2.dir/GameEngine/Damagable/Damagable.cpp.o"
+  "/home/cmuth/Github/G-JAM-001-STG-0-1-jam-louis.kleiver/GameEngine/Damage/Damage.cpp" "/home/cmuth/Github/G-JAM-001-STG-0-1-jam-louis.kleiver/CMakeFiles/superBonobros2.dir/GameEngine/Damage/Damage.cpp.o"
+  "/home/cmuth/Github/G-JAM-001-STG-0-1-jam-louis.kleiver/GameEngine/ECS/AEntity.cpp" "/home/cmuth/Github/G-JAM-001-STG-0-1-jam-louis.kleiver/CMakeFiles/superBonobros2.dir/GameEngine/ECS/AEntity.cpp.o"
+  "/home/cmuth/Github/G-JAM-001-STG-0-1-jam-louis.kleiver/GameEngine/ECS/AGame.cpp" "/home/cmuth/Github/G-JAM-001-STG-0-1-jam-louis.kleiver/CMakeFiles/superBonobros2.dir/GameEngine/ECS/AGame.cpp.o"
+  "/home/cmuth/Github/G-JAM-001-STG-0-1-jam-louis.kleiver/GameEngine/ECS/AScene.cpp" "/home/cmuth/Github/G-JAM-001-STG-0-1-jam-louis.kleiver/CMakeFiles/superBonobros2.dir/GameEngine/ECS/AScene.cpp.o"
+  "/home/cmuth/Github/G-JAM-001-STG-0-1-jam-louis.kleiver/GameEngine/ECS/ASystem.cpp" "/home/cmuth/Github/G-JAM-001-STG-0-1-jam-louis.kleiver/CMakeFiles/superBonobros2.dir/GameEngine/ECS/ASystem.cpp.o"
+  "/home/cmuth/Github/G-JAM-001-STG-0-1-jam-louis.kleiver/GameEngine/ECS/ECS.cpp" "/home/cmuth/Github/G-JAM-001-STG-0-1-jam-louis.kleiver/CMakeFiles/superBonobros2.dir/GameEngine/ECS/ECS.cpp.o"
+  "/home/cmuth/Github/G-JAM-001-STG-0-1-jam-louis.kleiver/GameEngine/Error/Error.cpp" "/home/cmuth/Github/G-JAM-001-STG-0-1-jam-louis.kleiver/CMakeFiles/superBonobros2.dir/GameEngine/Error/Error.cpp.o"
+  "/home/cmuth/Github/G-JAM-001-STG-0-1-jam-louis.kleiver/GameEngine/Event/SfmlEvent.cpp" "/home/cmuth/Github/G-JAM-001-STG-0-1-jam-louis.kleiver/CMakeFiles/superBonobros2.dir/GameEngine/Event/SfmlEvent.cpp.o"
+  "/home/cmuth/Github/G-JAM-001-STG-0-1-jam-louis.kleiver/GameEngine/Graphic/SFMLGraphic.cpp" "/home/cmuth/Github/G-JAM-001-STG-0-1-jam-louis.kleiver/CMakeFiles/superBonobros2.dir/GameEngine/Graphic/SFMLGraphic.cpp.o"
+  "/home/cmuth/Github/G-JAM-001-STG-0-1-jam-louis.kleiver/GameEngine/Network/Socket/SfmlUdpSocket.cpp" "/home/cmuth/Github/G-JAM-001-STG-0-1-jam-louis.kleiver/CMakeFiles/superBonobros2.dir/GameEngine/Network/Socket/SfmlUdpSocket.cpp.o"
+  "/home/cmuth/Github/G-JAM-001-STG-0-1-jam-louis.kleiver/GameEngine/Network/UdpNetwork.cpp" "/home/cmuth/Github/G-JAM-001-STG-0-1-jam-louis.kleiver/CMakeFiles/superBonobros2.dir/GameEngine/Network/UdpNetwork.cpp.o"
+  "/home/cmuth/Github/G-JAM-001-STG-0-1-jam-louis.kleiver/GameEngine/Spritesheet/SpritesheetAnimation.cpp" "/home/cmuth/Github/G-JAM-001-STG-0-1-jam-louis.kleiver/CMakeFiles/superBonobros2.dir/GameEngine/Spritesheet/SpritesheetAnimation.cpp.o"
+  "/home/cmuth/Github/G-JAM-001-STG-0-1-jam-louis.kleiver/GameEngine/Spritesheet/SpritesheetAnimators.cpp" "/home/cmuth/Github/G-JAM-001-STG-0-1-jam-louis.kleiver/CMakeFiles/superBonobros2.dir/GameEngine/Spritesheet/SpritesheetAnimators.cpp.o"
+  "/home/cmuth/Github/G-JAM-001-STG-0-1-jam-louis.kleiver/GameEngine/Systems/NetworkSystem.cpp" "/home/cmuth/Github/G-JAM-001-STG-0-1-jam-louis.kleiver/CMakeFiles/superBonobros2.dir/GameEngine/Systems/NetworkSystem.cpp.o"
+  "/home/cmuth/Github/G-JAM-001-STG-0-1-jam-louis.kleiver/GameEngine/Transform/Transform2D.cpp" "/home/cmuth/Github/G-JAM-001-STG-0-1-jam-louis.kleiver/CMakeFiles/superBonobros2.dir/GameEngine/Transform/Transform2D.cpp.o"
+  "/home/cmuth/Github/G-JAM-001-STG-0-1-jam-louis.kleiver/GameEngine/Velocity/Velocity.cpp" "/home/cmuth/Github/G-JAM-001-STG-0-1-jam-louis.kleiver/CMakeFiles/superBonobros2.dir/GameEngine/Velocity/Velocity.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "Game"
+  "GameEngine/Clock"
+  "GameEngine/Collision"
+  "GameEngine/Command"
+  "GameEngine/Components"
+  "GameEngine/Damagable"
+  "GameEngine/Damage"
+  "GameEngine/ECS"
+  "GameEngine/Entities"
+  "GameEngine/Error"
+  "GameEngine/Event"
+  "GameEngine/Graphic"
+  "GameEngine/Network"
+  "GameEngine/Network/Socket"
+  "GameEngine/Spritesheet"
+  "GameEngine/Systems"
+  "GameEngine/Transform"
+  "GameEngine/Types"
+  "GameEngine/Velocity"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/superBonobros2.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/superBonobros2.dir/Game/main.cpp.o"
+  "CMakeFiles/superBonobros2.dir/GameEngine/Clock/StdClock.cpp.o"
+  "CMakeFiles/superBonobros2.dir/GameEngine/Collision/SAT.cpp.o"
+  "CMakeFiles/superBonobros2.dir/GameEngine/Command/ASceneCommandInterpreter.cpp.o"
+  "CMakeFiles/superBonobros2.dir/GameEngine/Components/SkeletonSonsComponent.cpp.o"
+  "CMakeFiles/superBonobros2.dir/GameEngine/Damagable/Damagable.cpp.o"
+  "CMakeFiles/superBonobros2.dir/GameEngine/Damage/Damage.cpp.o"
+  "CMakeFiles/superBonobros2.dir/GameEngine/ECS/AEntity.cpp.o"
+  "CMakeFiles/superBonobros2.dir/GameEngine/ECS/AGame.cpp.o"
+  "CMakeFiles/superBonobros2.dir/GameEngine/ECS/AScene.cpp.o"
+  "CMakeFiles/superBonobros2.dir/GameEngine/ECS/ASystem.cpp.o"
+  "CMakeFiles/superBonobros2.dir/GameEngine/ECS/ECS.cpp.o"
+  "CMakeFiles/superBonobros2.dir/GameEngine/Error/Error.cpp.o"
+  "CMakeFiles/superBonobros2.dir/GameEngine/Event/SfmlEvent.cpp.o"
+  "CMakeFiles/superBonobros2.dir/GameEngine/Graphic/SFMLGraphic.cpp.o"
+  "CMakeFiles/superBonobros2.dir/GameEngine/Network/Socket/SfmlUdpSocket.cpp.o"
+  "CMakeFiles/superBonobros2.dir/GameEngine/Network/UdpNetwork.cpp.o"
+  "CMakeFiles/superBonobros2.dir/GameEngine/Spritesheet/SpritesheetAnimation.cpp.o"
+  "CMakeFiles/superBonobros2.dir/GameEngine/Spritesheet/SpritesheetAnimators.cpp.o"
+  "CMakeFiles/superBonobros2.dir/GameEngine/Systems/NetworkSystem.cpp.o"
+  "CMakeFiles/superBonobros2.dir/GameEngine/Transform/Transform2D.cpp.o"
+  "CMakeFiles/superBonobros2.dir/GameEngine/Velocity/Velocity.cpp.o"
+  "superBonobros2"
+  "superBonobros2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/superBonobros2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
